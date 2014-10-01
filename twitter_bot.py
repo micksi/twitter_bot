c7237cd40f53e5b392774d9ea2ebb874d07bcdcd
Mick Neupart <mickneupart@gmail.com>
2014-10-01 19:47:56 +0200

    added auto os detection for dataPath

--- twitter_bot.py
+++ twitter_bot.py
@@ -1,10 +1,16 @@
 import twitter_functions as tf
+from sys import platform as _platform
 
-#AmazonEC2
-dataPath = '/home/ubuntu/twitter_bot/data/'
+if _platform == "linux" or _platform == "linux2":
+    # linux - AmazonEC2
+	dataPath = '/home/ubuntu/twitter_bot/data/'
+elif _platform == "darwin":
+    # OS X - Local
+	dataPath = '/Users/WorkMachine/GDrive/DTU/socialGraphs/twitter_bot/data/'
+elif _platform == "win32":
+	pass
 
-#Local
-#dataPath = '/Users/WorkMachine/GDrive/DTU/socialGraphs/twitter_bot/data/'
+
 
 jamesLee = tf.TwitterUser(CONSUMER_KEY 			= 'wj5GCsbfLi5hIJLfKPxLHuD4g',
 						  CONSUMER_SECRET 		= 'TQ7tp7QNr7VdnWhj1WxLLoEVqFBQWDbL9sgB9Ujy3YLvZi9O5H',
